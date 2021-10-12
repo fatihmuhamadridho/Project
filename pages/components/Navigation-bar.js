04b95334ab656dfbde79f84cb43b1cd000da1bbb
Fatih <65400648+fatihmuhamadridho@users.noreply.github.com>
2021-10-12 21:13:37 +0700

    Update Navigation-bar.js

--- pages/components/Navigation-bar.js
+++ pages/components/Navigation-bar.js
@@ -1,4 +1,5 @@
 import styles from "../../styles/Home.module.scss";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
@@ -8,6 +9,11 @@ const Navbar = () => {
       </div>
 
       <div className={styles.sidenav}>
+        <div className={styles.logo}>
+          <Image src="/icon.png" width={50} height={40} />
+          <h3>Fake Store</h3>
+        </div>
+
         <a href="../Products">Products</a>
         <a href="../Users">User</a>
         <a href="../Carts">Cart User</a>
